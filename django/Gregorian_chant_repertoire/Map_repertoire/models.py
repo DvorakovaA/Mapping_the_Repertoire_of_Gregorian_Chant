ddf1539ad6492f65b4a348306d0eeff608f97030
Anna Dvořáková <98867919@cuni.cz>
2023-12-30 21:59:50 +0100

    feat: get database models and loading script ready

--- django/Gregorian_chant_repertoire/Map_repertoire/models.py
+++ django/Gregorian_chant_repertoire/Map_repertoire/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 class Data_Chant(models.Model):
     cantus_id = models.CharField(("cantus_id"), max_length=20)
-    siglum = models.CharField(("siglum"), max_length=20, null=True)
     feast_id = models.CharField(("feast_id"), max_length=20)
     source_id = models.CharField(("source_id"), max_length=500, null=True)
 
 
-class Source(models.Model):
+class Sources(models.Model):
     title = models.CharField(max_length=200)
     provenance_id = models.CharField(("provenance_id"), max_length=20, null=True)
     century = models.CharField(("century"), max_length=50, null=True)
+    siglum = models.CharField(("siglum"), max_length=20, null=True)
     drupal_path = models.CharField(("drupal_path"), max_length=500, null=True)
 
 
@@ -20,3 +20,9 @@ class Geography(models.Model):
     provenance = models.CharField(("provenance"), max_length=500, null=True)
     latitude = models.FloatField(("latitude"))
     longitude = models.FloatField(("longitude"))
+
+
+class Feasts(models.Model):
+    id = models.CharField(("feast_id"), max_length=20, primary_key=True)
+    feast_code = models.CharField(("feast_code"), max_length=20)
+    name = models.CharField(("name"), max_length=500)
---
+++ django/Gregorian_chant_repertoire/scripts/load_csv.py
@@ -0,0 +1,102 @@
+'''
+Python script that loads data from csv to django databases
+Data_Chant
+Sources
+Geography
+Feasts
+
+Loads only chants from surces where we have more than 100 chants from that source
+'''
+
+from Map_repertoire.models import Data_Chant, Sources, Geography, Feasts
+from django.conf import settings
+import pandas as pd
+
+def run():
+    '''
+    Main function of script load_csv.py that loads data from csv (merged, filtered) to django database
+    '''
+    # CSV ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Get dataframes ready
+    antiphones = pd.read_csv('all-ci-antiphons.csv', usecols=['cantus_id', 'feast_id', 'drupal_path'])  # converters={'cantus_id' : str})
+    responsories = pd.read_csv('all-ci-responsories.csv', usecols=['cantus_id', 'feast_id', 'drupal_path'])
+    sources = pd.read_csv('sources-of-all-ci-antiphones.csv', usecols=['title', 'siglum', 'provenance_id', 'century', 'drupal_path'])
+    geography = pd.read_csv('provenances_data.csv', usecols=['provenance_id', 'provenance', 'latitude', 'longitude'])
+    feasts = pd.read_csv('feast.csv', usecols=['id', 'name', 'feast_code'])
+
+    # Merge antiphons and responsories
+    chant_data = pd.concat([antiphones, responsories])
+    
+    # Filter sources to use only those with more than 100 chants
+    freq_of_sources = chant_data['drupal_path'].value_counts()
+    bigger_sources = freq_of_sources.drop(freq_of_sources[freq_of_sources.values < 100].index).index.tolist()
+    sources_f = sources[sources['drupal_path'].isin(bigger_sources)]
+
+    # Filter feasts to use onlz those used in filtered sources
+    freq_of_feasts = chant_data['feast_id'].value_counts()
+    print("number of all feasts in bigger sources:", len(freq_of_feasts))
+    bigger_feasts = freq_of_feasts.drop(freq_of_feasts[freq_of_feasts.values < 10].index).index.tolist()
+    print("number of all bigger feasts in bigger sources:", len(bigger_sources))
+    feasts_f = feasts[feasts['id'].isin(bigger_feasts)]
+
+    # Filter chants based on filtered sources
+    chant_data_f = chant_data[chant_data['drupal_path'].isin(sources_f['drupal_path'])]
+
+
+    # Database ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Get ready databases
+    Data_Chant.objects.all().delete()
+    Sources.objects.all().delete()
+    Geography.objects.all().delete()
+    Feasts.objects.all().delete()
+
+    # Fill Chant info
+    row_iter = chant_data_f.iterrows()
+    objs = [
+        Data_Chant( 
+            cantus_id=row['cantus_id'],
+            feast_id=row['feast_id'],
+            source_id=row['drupal_path']
+        )
+        for index, row in row_iter
+    ]
+    Data_Chant.objects.bulk_create(objs)
+
+    # Fill Sources info
+    row_iter = sources_f.iterrows()
+    objs = [
+        Sources(
+            title=row['title'],
+            provenance_id=row['provenance_id'],
+            century=row['century'],
+            siglum=row['siglum'],
+            drupal_path=row['drupal_path']
+        )
+        for index, row in row_iter
+    ]
+    Sources.objects.bulk_create(objs)
+
+    # Fill Geography info
+    row_iter = geography.iterrows()
+    objs = [
+        Geography(
+            provenance_id=row['provenance_id'],
+            provenance=row['provenance'],
+            latitude=row['latitude'],
+            longitude=row['longitude']
+        )
+        for index, row in row_iter
+    ]
+    Geography.objects.bulk_create(objs)
+
+    # Fill Feasts info
+    row_iter = geography.iterrows()
+    objs = [
+        Feasts(
+            feast_id=row['id'],
+            name=row['name'],
+            feast_code=row['feast_code']
+        )
+        for index, row in row_iter
+    ]
+    Feasts.objects.bulk_create(objs)
