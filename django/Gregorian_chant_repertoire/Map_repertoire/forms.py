c4be74314d4dc4abd3b5a1dc01194f49fa97951a
Anna Dvořáková <98867919@cuni.cz>
2023-12-31 20:59:57 +0100

    feat: add sample input form

---
+++ django/Gregorian_chant_repertoire/Map_repertoire/forms.py
@@ -0,0 +1,8 @@
+"""
+
+"""
+from django import forms
+
+
+class InputForm(forms.Form):
+    feast = forms.ChoiceField(choices=[['1', 'Anezka'], ['2', 'Vojtech']])
--- django/Gregorian_chant_repertoire/Map_repertoire/views.py
+++ django/Gregorian_chant_repertoire/Map_repertoire/views.py
@@ -1,9 +1,22 @@
+"""
+"""
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import InputForm
 
 
 def index(request):
-    return render(request, "Map_repertoire/index.html")
+    '''
+    '''
+    #context = {}
+    #if request.method == "POST":
+    form = InputForm(request.POST or None)
+    if form.is_valid():
+        request.session['feast'] = form.cleaned_data['feast']
+        return HttpResponseRedirect('', request)
+    context = {"form": form}
+    return render(request, "Map_repertoire/index.html", context)
 
 
 def help(request):
