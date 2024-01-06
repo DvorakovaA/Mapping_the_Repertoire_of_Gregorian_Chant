f689a4a1463769699483f73329d2397c28660f6b
Anna Dvořáková <98867919@cuni.cz>
2024-01-06 20:43:49 +0100

    fix: add flag to show result of request from form only when asked

--- django/Gregorian_chant_repertoire/Map_repertoire/forms.py
+++ django/Gregorian_chant_repertoire/Map_repertoire/forms.py
@@ -6,5 +6,4 @@ from django import forms
 from .models import Feasts
 
 class InputForm(forms.Form):
-    feast = forms.ChoiceField(initial='Select feast',
-                              choices=[(f[0], f[1]) for f in zip([i for i in range(len(Feasts.objects.all()))], Feasts.objects.values_list('name', flat=True))])
+    feast = forms.ChoiceField(choices=[(f[0], f[1]) for f in zip([i for i in range(len(Feasts.objects.all()))], Feasts.objects.values_list('name', flat=True))])
--- django/Gregorian_chant_repertoire/Map_repertoire/views.py
+++ django/Gregorian_chant_repertoire/Map_repertoire/views.py
@@ -15,23 +15,27 @@ def index(request):
     """
     Function that manages main html page of app - displays input form and shows results (table and map)
     """
-    #context = {}
+    context = {}
     #if request.method == "POST":
     form = InputForm(request.POST or None, initial={'feast' : '---'})
     if form.is_valid():
         request.session['feast'] = form.cleaned_data['feast']
+        request.session['REQUESTED'] = True
         return HttpResponseRedirect('', request)
+        
 
     context = { "form": form }
-
-    feast_name = Feasts.objects.values_list('name', flat=True)[int(request.session.get('feast'))]
-    context['feast'] = feast_name
-    feast_id = Feasts.objects.filter(name = feast_name).values()[0]['feast_id']
-    context['feast_id'] = feast_id
-    communities = get_communities(feast_id)
-    context['map'] = get_map(communities)._repr_html_()
-
-    context['sources'] = [{'drupal_path' : source} for setik in communities for source in setik ]
+    if request.session.get('REQUESTED'):
+        feast_name = Feasts.objects.values_list('name', flat=True)[int(request.session.get('feast'))]
+        context['feast'] = feast_name
+        feast_id = Feasts.objects.filter(name = feast_name).values()[0]['feast_id']
+        context['feast_id'] = feast_id
+        communities = get_communities(feast_id)
+        context['map'] = get_map(communities)._repr_html_()
+        
+        context['sources'] = [{'drupal_path' : source} for com in communities for source in com]
+        request.session['REQUESTED'] = False
+    
     return render(request, "Map_repertoire/index.html", context)
 
 
