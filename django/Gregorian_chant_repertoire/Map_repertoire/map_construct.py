9af1a6a12d10f3fac28c31619af7d992a426baa5
Anna Dvořáková <98867919@cuni.cz>
2024-02-09 10:45:26 +0100

    feat: add edges displaying to century map

--- django/Gregorian_chant_repertoire/Map_repertoire/map_construct.py
+++ django/Gregorian_chant_repertoire/Map_repertoire/map_construct.py
@@ -1,7 +1,8 @@
-"""
-Module providing main function get_map
-that returns html of folium map visualization of given sources communities
-"""
+'''
+Module providing main function get_maps
+that returns two objects of folium maps visualization of given sources communities,
+one for community display and one for century display
+'''
 
 import folium
 from folium.plugins import MarkerCluster
@@ -14,6 +15,12 @@ map_centre = [47.466667, 11.166667] # Centre of returned maps
 
 
 def get_maps(communities : list[set [str]], edges : list [tuple]):
+    '''
+    Given communities of sources and info about conections beetween sources
+    function constructs two folium map objects one with community layers and one
+    with layers based on centuries of origin
+    '''
+    # Get map objects and assign them map layer
     com_map = folium.Map(map_centre, zoom_start=5, height='70%', width='70%', tiles=None)
     tile_layer1 = folium.TileLayer(control=False)
     tile_layer1.add_to(com_map)
@@ -32,29 +39,25 @@ def get_maps(communities : list[set [str]], edges : list [tuple]):
         color=rgb2hex(cmap(scale))
         colors.append(color)
     
-    # Get dict of sources in communities
+    # Get dict (-> source : info_about_source) for sources in communities
     sources_dict = {}
-    i = 0
     for community in communities:
         for source in community:
-            sources_dict[source] = i
-            i += 1
+            sources_dict[source] = Sources.objects.filter(drupal_path = source).values()[0]
 
-    used_sources = [Sources.objects.filter(drupal_path = source).values()[0] for source in sources_dict.keys()]
-    num_centuries = [s['num_century'] for s in used_sources]
-    provenance_ids = [s['provenance_id'] for s in used_sources]
-    centuries = [s['century'] for s in used_sources]
-    siglums = [s['siglum'] for s in used_sources]
 
-    # Layers
+    # Displaying layers and dicts that hold them
+    numeric_centuries = [sources_dict[s]['num_century'] for s in sources_dict.keys()]
+    numeric_centuries.sort()
     century_layers = {}
-    for century in num_centuries:
-        c_layer = folium.FeatureGroup(name=str(century) + ". century", show=True)
-        century_layers[century] = c_layer
+    for numeric_cen in numeric_centuries:
+        c_layer = folium.FeatureGroup(name=str(numeric_cen) + ". century", show=True)
+        century_layers[numeric_cen] = c_layer
         cen_map.add_child(c_layer)
 
     community_layers = {}
-    sources_coordinates = {}
+
+    coordinates_of_sources = {}
 
     # Points
     sources_with_no_geo = []
@@ -62,15 +65,15 @@ def get_maps(communities : list[set [str]], edges : list [tuple]):
     for community in communities:
         com = folium.FeatureGroup(name="Community " + str(i+1) , show=True)
         for source in community:
-            cen = century_layers[num_centuries[j]]
+            cen = century_layers[numeric_centuries[j]]
             community_layers[source] = (com, i)
-            if Geography.objects.filter(provenance_id = provenance_ids[j]).exists():
-                place = Geography.objects.filter(provenance_id = provenance_ids[j]).values()
+            if Geography.objects.filter(provenance_id = sources_dict[source]['provenance_id']).exists():
+                place = Geography.objects.filter(provenance_id = sources_dict[source]['provenance_id']).values()
                 lat = place[0]['latitude']
                 long = place[0]['longitude']
-                sources_coordinates[source] = [lat, long]
-                provenance = Sources.objects.filter(provenance_id = provenance_ids[j]).values()[0]['provenance']
-                info = "<h5> <a href=  \"{}\"  target=\"_blank\" rel=\"noopener noreferrer\"> {} </a>".format(source, source) + "</h5> <h5> Provenance: " + provenance + "</h5> <h5> Century: " + centuries[j] + "</h5> <h5> Siglum: " + siglums[j] + "</h5>"
+                coordinates_of_sources[source] = [lat, long]
+                provenance = Sources.objects.filter(provenance_id = sources_dict[source]['provenance_id']).values()[0]['provenance']
+                info = "<h5> <a href=  \"{}\"  target=\"_blank\" rel=\"noopener noreferrer\"> {} </a>".format(source, source) + "</h5> <h5> Provenance: " + provenance + "</h5> <h5> Century: " + sources_dict[source]['century'] + "</h5> <h5> Siglum: " + sources_dict[source]['siglum'] + "</h5>"
                 popup1 = folium.Popup(info, max_width=300, min_width =300)
                 popup2 = folium.Popup(info, max_width=300, min_width =300)
 
@@ -92,10 +95,14 @@ def get_maps(communities : list[set [str]], edges : list [tuple]):
     # Lines
     for line in edges:
         if line[0] not in sources_with_no_geo and line[1] not in sources_with_no_geo: # there is a way of drawing it
-            coord_1 = sources_coordinates[line[0]]
-            coord_2 = sources_coordinates[line[1]]
+            coord_1 = coordinates_of_sources[line[0]]
+            coord_2 = coordinates_of_sources[line[1]]
+
             popup1 = folium.Popup('<h5> Jaccard distance: ' + str(line[2]['weight']) + '</h5>', max_width=300)
             popup2 = folium.Popup('<h5> Jaccard distance: ' + str(line[2]['weight']) + '</h5>', max_width=300)
+            popup3 = folium.Popup('<h5> Jaccard distance: ' + str(line[2]['weight']) + '</h5>', max_width=300)
+            popup4 = folium.Popup('<h5> Jaccard distance: ' + str(line[2]['weight']) + '</h5>', max_width=300)
+
             if community_layers[line[0]][1] == community_layers[line[1]][1]:
                 color = colors[community_layers[line[0]][1]]
                 com = community_layers[line[0]][0]
@@ -107,7 +114,18 @@ def get_maps(communities : list[set [str]], edges : list [tuple]):
                 folium.PolyLine(locations=[coord_1, coord_2], weight=line[2]['weight']*6, popup=popup1, color=color).add_to(com1)
                 folium.PolyLine(locations=[coord_1, coord_2], weight=line[2]['weight']*6, popup=popup2, color=color).add_to(com2)
             
-    
+            if sources_dict[line[0]]['num_century'] == sources_dict[line[1]]['num_century']:
+                color = 'black'
+                cen = century_layers[sources_dict[line[0]]['num_century']]
+                folium.PolyLine(locations=[coord_1, coord_2], weight=line[2]['weight']*5, popup=popup3, color=color).add_to(cen)
+            else:
+                color = 'black'
+                cen1 = century_layers[sources_dict[line[0]]['num_century']]
+                cen2 = century_layers[sources_dict[line[1]]['num_century']]
+                folium.PolyLine(locations=[coord_1, coord_2], weight=line[2]['weight']*5, popup=popup3, color=color).add_to(cen1)
+                folium.PolyLine(locations=[coord_1, coord_2], weight=line[2]['weight']*5, popup=popup4, color=color).add_to(cen2)
+
+
     folium.LayerControl(collapsed=False).add_to(com_map)
     folium.LayerControl(collapsed=False).add_to(cen_map)
     return com_map, cen_map
