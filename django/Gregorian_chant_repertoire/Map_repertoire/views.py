ef643ef1919c5d5cda5b2c9133d7bd62e91fb823
Anna Dvořáková <98867919@cuni.cz>
2024-02-17 20:57:06 +0100

    fix: remove not needed redirect on index page

--- django/Gregorian_chant_repertoire/Map_repertoire/views.py
+++ django/Gregorian_chant_repertoire/Map_repertoire/views.py
@@ -17,29 +17,24 @@ def index(request):
     Function that manages main html page of app - displays input form and shows results (table and map)
     """
     context = {}
-    #if request.method == "POST":
+
     form = InputForm(request.POST or None, initial={'feast' : '---'})
     if form.is_valid():
         request.session['feast'] = form.cleaned_data['feast']
-        request.session['REQUESTED'] = True
-        return HttpResponseRedirect('', request)
         
+    context = { "form" : form }
+    feast_name = Feasts.objects.values_list('name', flat=True)[int(request.session.get('feast'))]
+    context['feast'] = feast_name
+    feast_id = Feasts.objects.filter(name = feast_name).values()[0]['feast_id']
+    context['feast_id'] = [feast_id]
 
-    context = { "form": form }
-    if request.session.get('REQUESTED'):
-        feast_name = Feasts.objects.values_list('name', flat=True)[int(request.session.get('feast'))]
-        context['feast'] = feast_name
-        feast_id = Feasts.objects.filter(name = feast_name).values()[0]['feast_id']
-        context['feast_id'] = [feast_id]
-
-        communities, edges_info = get_communities([feast_id])
-        
-        com_map, cen_map = get_maps(communities, edges_info)
-        context['com_map'] = com_map._repr_html_()
-        context['cen_map'] = cen_map._repr_html_()
-        
-        context['tab_data'] = get_table(communities, [feast_id])
-        request.session['REQUESTED'] = False
+    communities, edges_info = get_communities([feast_id])
+    
+    com_map, cen_map = get_maps(communities, edges_info)
+    context['com_map'] = com_map._repr_html_()
+    context['cen_map'] = cen_map._repr_html_()
+    
+    context['tab_data'] = get_table(communities, [feast_id])
     
     return render(request, "map_repertoire/index.html", context)
 
