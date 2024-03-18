f3233b58e2ca8d0e3b89116ff517830e7dbcb861
Anna Dvořáková <98867919@cuni.cz>
2024-03-18 21:07:37 +0100

    feat: create admin user and activate admin site to inspect database human-readable way easily

--- django/Gregorian_chant_repertoire/Map_repertoire/admin.py
+++ django/Gregorian_chant_repertoire/Map_repertoire/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Feasts, Sources, Data_Chant, Geography
+
+admin.site.register(Feasts)
+admin.site.register(Sources)
+admin.site.register(Data_Chant)
+admin.site.register(Geography)
