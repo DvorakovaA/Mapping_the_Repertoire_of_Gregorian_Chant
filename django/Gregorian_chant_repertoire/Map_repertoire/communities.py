8291c68a4765778472f1c8aa541d9b72edd82239
Anna Dvořáková <98867919@cuni.cz>
2024-05-21 13:35:02 +0200

    feat: add All feast and 2 topics options processing

--- django/Gregorian_chant_repertoire/Map_repertoire/communities.py
+++ django/Gregorian_chant_repertoire/Map_repertoire/communities.py
@@ -42,22 +42,28 @@ def find_comms_stability(community_versions : list[list[set[str]]]):
                 source_to_id[s] = i
                 i += 1
 
-        friends_matrix = np.zeros([len(source_to_id), len(source_to_id)])
-
-        for com_ver in community_versions:
-            for com in com_ver:
-                com = sorted(list(com))
-                for pair in combinations(com, 2):
-                    friends_matrix[source_to_id[pair[0]], source_to_id[pair[1]]] += 1
-
-        if np.count_nonzero(friends_matrix) > len(source_to_id)*len(source_to_id):
-            sig_level = round((np.sum(friends_matrix) / np.count_nonzero(friends_matrix)) / len(community_versions), 2)
-        else:
-            sig_level = 1
-
-        return community_versions[0], float(sig_level)
+        jaccard = []
+        all_variants_pairs = [(a, b) for idx, a in enumerate(community_versions) for b in community_versions[idx + 1:]]
+        for pair in all_variants_pairs:
+            friends_matrix = np.zeros([len(source_to_id), len(source_to_id)])
+            for com_ver in pair:
+                for com in com_ver:
+                    com = sorted(list(com))
+                    for s_pair in combinations(com, 2):
+                        friends_matrix[source_to_id[s_pair[0]], source_to_id[s_pair[1]]] += 1
+            unique, counts = np.unique(friends_matrix.flatten(), return_counts=True)
+            try:
+                twos = dict(zip(unique, counts))[2]
+                sig_level = twos / np.count_nonzero(friends_matrix)
+            except:
+                sig_level = 0.0
+            jaccard.append(sig_level)
+        
+        return community_versions[0], (round(np.mean(jaccard), 2)) 
+        
     else:
         return [], 0
+
 
 
 def get_network_info(feast_ids : list[str], compare_metrics, filtering_office : list[str], get_shared : bool):
@@ -75,22 +81,35 @@ def get_network_info(feast_ids : list[str], compare_metrics, filtering_office : 
     if filtering_office != []:
         DO_FILTER_OFFICE = True
  
-    chants_of_feasts = []
-    for feast_id in feast_ids:
-        chants_of_feasts += Data_Chant.objects.filter(feast_id = feast_id).values()
+    if feast_ids == ['All']:
+        # Construct needed data structures to construct networks from all data
+        for source_id in drupals:
+            if DO_FILTER_OFFICE:
+                chants_of_source = []
+                for office_id in filtering_office:
+                    chants_of_source += [chant[0] for chant in Data_Chant.objects.filter(source_id = source_id[0], office_id = office_id).values_list('cantus_id')]
+            else:        
+                chants_of_source = [chant[0] for chant in Data_Chant.objects.filter(source_id = source_id[0]).values_list('cantus_id')]  #Data_Chant.objects.filter(source_id = source_id[0]).values_list('cantus_id')[0] #
+            if chants_of_source != []:
+                used_sources.append(source_id[0])
+                source_chants_dict[source_id[0]] = chants_of_source
+    else:
+        chants_of_feasts = []
+        for feast_id in feast_ids:
+           chants_of_feasts += Data_Chant.objects.filter(feast_id = feast_id).values()
 
-    for source_id in drupals:
-        if DO_FILTER_OFFICE:
-            chants_of_source = []
-            for office in filtering_office:
-                chants_of_source += [chant['cantus_id'] for chant in chants_of_feasts if chant['source_id'] == source_id[0] and chant['office_id'] == office]
-        else:
-            chants_of_source = [chant['cantus_id'] for chant in chants_of_feasts if chant['source_id'] == source_id[0]]
+        for source_id in drupals:
+            if DO_FILTER_OFFICE:
+                chants_of_source = []
+                for office in filtering_office:
+                    chants_of_source += [chant['cantus_id'] for chant in chants_of_feasts if chant['source_id'] == source_id[0] and chant['office_id'] == office]
+            else:
+                chants_of_source = [chant['cantus_id'] for chant in chants_of_feasts if chant['source_id'] == source_id[0]]
 
-        if chants_of_source != []:
-            used_sources.append(source_id[0])
-            source_chants_dict[source_id[0]] = chants_of_source
-
+            if chants_of_source != []:
+                used_sources.append(source_id[0])
+                source_chants_dict[source_id[0]] = chants_of_source
+    
     used_sources = list(set(used_sources))
     len_s = len(used_sources)
     s1_column = [j for i in [len_s * [s] for s in used_sources] for j in i]
@@ -109,7 +128,7 @@ def get_network_info(feast_ids : list[str], compare_metrics, filtering_office : 
                 ch2_column.append(len(s2_chants))
                 shared_column.append(Jaccard_metrics(s1_chants, s2_chants))
             edges = [(i, j, {'weight': round(w, 2) }) for i, j, w in zip(s1_column, s2_column, shared_column) if i != j and w != 0 and (i in used_sources and j in used_sources)]
-        else: # Comparison based on topic model
+        else: # Comparison distance based on topic model
             with lzma.open('Map_repertoire/topic_models/dist_reduction.model', "rb") as model_file:
                 trans = pickle.load(model_file)
                 model = pickle.load(model_file)
@@ -127,7 +146,7 @@ def get_network_info(feast_ids : list[str], compare_metrics, filtering_office : 
                     shared_column.append(jensenshannon(result[0], result[1]))
             edges = []
 
-        # join obtained data into structure for graph and map
+        # Join obtained data into structure for graph and map
         edges_info = [(s1, s2, ch1, ch2, {'weight': round(w, 2) }) for s1, s2, ch1, ch2, w in zip(s1_column, s2_column, ch1_column, ch2_column, shared_column) if s1 != s2 and w != 0 and (s1 in used_sources and s2 in used_sources)]
     
     else: # We have topic model based clustering
@@ -141,6 +160,7 @@ def get_network_info(feast_ids : list[str], compare_metrics, filtering_office : 
         edges = []
         edges_info = [(s1, s2, ch1, ch2, {'weight': w}) for s1, s2, ch1, ch2, w in zip(s1_column, s2_column, ch1_column, ch2_column, shared_column) if s1 != s2 and w != 0 and (s1 in used_sources and s2 in used_sources)]
     
+    print('constructed')
     return source_chants_dict, edges_info, edges, used_sources
 
 
@@ -165,9 +185,10 @@ def get_louvein_communities(feast_ids : list[str], filtering_office : list[str],
     about stability and about edges for map construction
     '''
     graph, edges_info = get_graph(feast_ids=feast_ids, filtering_office=filtering_office, metric=add_info_algo)
-        
+    
+    LOUVEIN_NUM_OF_RUNS = 10
     community_versions = []
-    for _ in range(10):
+    for _ in range(LOUVEIN_NUM_OF_RUNS):
         community_versions.append(nx.community.louvain_communities(graph, weight='weight'))
     
     communities, sig_level = find_comms_stability(community_versions)
@@ -191,7 +212,7 @@ def get_distance_matrix(source_chants_dict, used_sources, metric : str):
     if metric == 'Jaccard':
         for s_i in used_sources:
             for s_j in used_sources:
-                distance_matrix[source_chants_dict[s_i], source_chants_dict[s_j]] = 1 - Jaccard_metrics(source_chants_dict[s_i], source_chants_dict[s_j])
+                distance_matrix[source_dict[s_i], source_dict[s_j]] = 1 - Jaccard_metrics(source_chants_dict[s_i], source_chants_dict[s_j])
     
     else: # Comparison based on topic model
         with lzma.open('Map_repertoire/topic_models/dist_reduction.model', "rb") as model_file:
@@ -229,8 +250,9 @@ def get_dbscan_communities(feast_ids : list[str], filtering_office : list[str], 
 
     EPS = 0.2
     MIN_SAMPLES = 3
+    DBSCAN_NUM_OF_RUNS = 2
     community_versions = []
-    for _ in range(5):
+    for _ in range(DBSCAN_NUM_OF_RUNS):
         perm = list(range(len(used_sources)))
         random.shuffle(perm)
         shuff_sources = shuffle(perm, used_sources)
@@ -259,7 +281,11 @@ def get_topic_model_communities(feast_ids : list[str], filtering_office : list[s
     source_chants_dict, edges_info, _, used_sources = get_network_info(feast_ids=feast_ids, filtering_office=filtering_office, compare_metrics="", get_shared=False)
 
     # Get model
-    if add_info_algo == '5':
+    if add_info_algo == '2':
+        with lzma.open('Map_repertoire/topic_models/topic_2.model', "rb") as model_file:
+            trans = pickle.load(model_file)
+            model = pickle.load(model_file)
+    elif add_info_algo == '5':
         with lzma.open('Map_repertoire/topic_models/topic_5.model', "rb") as model_file:
             trans = pickle.load(model_file)
             model = pickle.load(model_file)
@@ -294,6 +320,7 @@ def get_communities(feast_ids : list[str], filtering_office : list[str], algorit
     returns communities found by Louvein algorithm, DBSCAN clustering or obtained from topic models 
     and info about edges of network to be drawn on the map as well as stability measure of clusterings
     """
+    print(add_info_algo)
     if algorithm == 'Louvein':
         communities, edges_info, sig_level = get_louvein_communities(feast_ids, filtering_office, add_info_algo)
     
