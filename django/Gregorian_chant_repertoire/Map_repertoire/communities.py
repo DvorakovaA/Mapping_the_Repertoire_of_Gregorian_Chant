eaea6add43380d5a6315ee23ef0442367aae06c7
Anna Dvořáková <98867919@cuni.cz>
2024-07-05 21:59:06 +0200

    feat: use stored data for All feasts and All offices option instead of constructing them from db

--- django/Gregorian_chant_repertoire/Map_repertoire/communities.py
+++ django/Gregorian_chant_repertoire/Map_repertoire/communities.py
@@ -9,17 +9,17 @@ import numpy as np
 import lzma
 import pickle
 import random
+import functools
 from itertools import combinations
 from scipy.spatial.distance import jensenshannon
 from sklearn.cluster import DBSCAN
 
 from .models import Data_Chant, Sources
-from django.core.files import File
 
 
 
 # Metric for measuring similarity of two sets ('chant sharingness')
-def Jaccard_metrics(a : list, b : list):
+def Jaccard_metric(a : list, b : list):
     '''
     Function returns value of Jaccard metrics applied on two sets
     '''
@@ -28,11 +28,18 @@ def Jaccard_metrics(a : list, b : list):
     else:
         return 0
 
+@functools.lru_cache(maxsize=None)
+def JensenShannon_metric(s1, s2):
+    return jensenshannon(s1, s2)
+
 
 def find_comms_stability(community_versions : list[list[set[str]]]):
     '''
-    For more versions of communities it counts Jaccard index and returns first given community
-    (brings info anout stability of clustering)
+    For more versions of communities it counts edited Jaccard score* of each pair 
+    and returns its mean and first given community as "stable" 
+        - because we do not have any principle to determine wich version is better than others
+    (brings info about stability of clustering)
+    * Jaccard index where we add number_of_samples to both nominator and denominator
     '''
     if community_versions != []:
         i = 0
@@ -54,12 +61,12 @@ def find_comms_stability(community_versions : list[list[set[str]]]):
             unique, counts = np.unique(friends_matrix.flatten(), return_counts=True)
             try:
                 twos = dict(zip(unique, counts))[2]
-                sig_level = twos / np.count_nonzero(friends_matrix)
+                sig_level = (twos + len(source_to_id)) / (np.count_nonzero(friends_matrix) + len(source_to_id))
             except:
-                sig_level = 0.0
+                sig_level = len(source_to_id) / (np.count_nonzero(friends_matrix) + len(source_to_id))
             jaccard.append(sig_level)
         
-        return community_versions[0], (round(np.mean(jaccard), 2)) 
+        return community_versions[0], round(np.mean(jaccard), 2)
         
     else:
         return [], 0
@@ -70,8 +77,8 @@ def get_network_info(feast_ids : list[str], compare_metrics, filtering_office : 
     """
     Function that constructs data structures that are
     used by clustering algorithms and for construction of graph on map 
-    based on given feast ids, metric to be used (Jaccard od ropic model comparison)
-    and offices to be considered
+    based on given feast ids, metric to be used (Jaccard od topic model comparison)
+    and offices to be considered -> returns: source_chants_dict, edges_info, edges, used_sources
     """
     drupals = Sources.objects.values_list('drupal_path')
     source_chants_dict = {}
@@ -80,87 +87,110 @@ def get_network_info(feast_ids : list[str], compare_metrics, filtering_office : 
     DO_FILTER_OFFICE = False
     if filtering_office != []:
         DO_FILTER_OFFICE = True
- 
-    if feast_ids == ['All']:
-        # Construct needed data structures to construct networks from all data
-        for source_id in drupals:
-            if DO_FILTER_OFFICE:
-                chants_of_source = []
-                for office_id in filtering_office:
-                    chants_of_source += [chant[0] for chant in Data_Chant.objects.filter(source_id = source_id[0], office_id = office_id).values_list('cantus_id')]
-            else:        
-                chants_of_source = [chant[0] for chant in Data_Chant.objects.filter(source_id = source_id[0]).values_list('cantus_id')]  #Data_Chant.objects.filter(source_id = source_id[0]).values_list('cantus_id')[0] #
-            if chants_of_source != []:
-                used_sources.append(source_id[0])
-                source_chants_dict[source_id[0]] = chants_of_source
-    else:
-        chants_of_feasts = []
-        for feast_id in feast_ids:
-           chants_of_feasts += Data_Chant.objects.filter(feast_id = feast_id).values()
-
-        for source_id in drupals:
-            if DO_FILTER_OFFICE:
-                chants_of_source = []
-                for office in filtering_office:
-                    chants_of_source += [chant['cantus_id'] for chant in chants_of_feasts if chant['source_id'] == source_id[0] and chant['office_id'] == office]
+    
+    # All all data
+    if feast_ids == ['All'] and not DO_FILTER_OFFICE:
+        with lzma.open("Map_repertoire/big_data_structures/all_source_chants_dict.txt", "rb") as file:
+            source_chants_dict = pickle.load(file)
+            used_sources = [source[0] for source in drupals]
+        if get_shared:
+            if compare_metrics == 'Jaccard':
+                with lzma.open("Map_repertoire/big_data_structures/all_jaccard_edges.txt", "rb") as file:
+                    edges = pickle.load(file)
+                with lzma.open("Map_repertoire/big_data_structures/all_jaccard_edges_info.txt", "rb") as file:
+                    edges_info = pickle.load(file)
             else:
-                chants_of_source = [chant['cantus_id'] for chant in chants_of_feasts if chant['source_id'] == source_id[0]]
-
-            if chants_of_source != []:
-                used_sources.append(source_id[0])
-                source_chants_dict[source_id[0]] = chants_of_source
-    
-    used_sources = list(set(used_sources))
-    len_s = len(used_sources)
-    s1_column = [j for i in [len_s * [s] for s in used_sources] for j in i]
-    s2_column = len_s * used_sources
-
-    shared_column = []
-    ch1_column = []
-    ch2_column = []
-
-    if get_shared:
-        if compare_metrics == 'Jaccard':
-            for i in range(len(s1_column)):
-                s1_chants = source_chants_dict[s1_column[i]]
-                s2_chants = source_chants_dict[s2_column[i]]
-                ch1_column.append(len(s1_chants))
-                ch2_column.append(len(s2_chants))
-                shared_column.append(Jaccard_metrics(s1_chants, s2_chants))
-            edges = [(i, j, {'weight': round(w, 2) }) for i, j, w in zip(s1_column, s2_column, shared_column) if i != j and w != 0 and (i in used_sources and j in used_sources)]
-        else: # Comparison distance based on topic model
-            with lzma.open('Map_repertoire/topic_models/dist_reduction.model', "rb") as model_file:
-                trans = pickle.load(model_file)
-                model = pickle.load(model_file)
-            
-            for i in range(len(s1_column)):
-                s1_chants = source_chants_dict[s1_column[i]]
-                s2_chants = source_chants_dict[s2_column[i]]
-                ch1_column.append(len(s1_chants))
-                ch2_column.append(len(s2_chants))
-                trans_data = trans.transform([' '.join(s1_chants), ' '.join(s2_chants)])
-                result = model.transform(trans_data)
-                if np.isnan(jensenshannon(result[0], result[1])):
-                    shared_column.append(0)
-                else:
-                    shared_column.append(jensenshannon(result[0], result[1]))
+                with lzma.open("Map_repertoire/big_data_structures/all_top_dist_edges.txt", "rb") as file:
+                    edges = pickle.load(file)
+                with lzma.open("Map_repertoire/big_data_structures/all_top_dist_edges_info.txt", "rb") as file:
+                    edges_info = pickle.load(file)
+        else:
             edges = []
+            with lzma.open("Map_repertoire/big_data_structures/all_topics_edges_info.txt", "rb") as file:
+                    edges_info = pickle.load(file)
 
-        # Join obtained data into structure for graph and map
-        edges_info = [(s1, s2, ch1, ch2, {'weight': round(w, 2) }) for s1, s2, ch1, ch2, w in zip(s1_column, s2_column, ch1_column, ch2_column, shared_column) if s1 != s2 and w != 0 and (s1 in used_sources and s2 in used_sources)]
-    
-    else: # We have topic model based clustering
-        for i in range(len(s1_column)):
-            s1_chants = source_chants_dict[s1_column[i]]
-            s2_chants = source_chants_dict[s2_column[i]]
-            ch1_column.append(len(s1_chants))
-            ch2_column.append(len(s2_chants))
-            shared_column.append('-')
+
+    else: # other selection than All feasts and All offices
+        if feast_ids == ['All']: # W 
+            # Construct needed data structures to construct networks from all data
+            for source_id in drupals:
+                chants_of_source = []
+                for office_id in filtering_office: # We have to pick only some office data
+                    chants_of_source += [chant[0] for chant in Data_Chant.objects.filter(source_id = source_id[0], office_id = office_id).values_list('cantus_id')]
+                
+                if chants_of_source != []:
+                    used_sources.append(source_id[0])
+                    source_chants_dict[source_id[0]] = chants_of_source
+        else: # Not All feasts requested
+            chants_of_feasts = []
+            for feast_id in feast_ids:
+                chants_of_feasts += Data_Chant.objects.filter(feast_id = feast_id).values()
+
+            for source_id in drupals:
+                if DO_FILTER_OFFICE:
+                    chants_of_source = []
+                    for office in filtering_office:
+                        chants_of_source += [chant['cantus_id'] for chant in chants_of_feasts if chant['source_id'] == source_id[0] and chant['office_id'] == office]
+                else:
+                    chants_of_source = [chant['cantus_id'] for chant in chants_of_feasts if chant['source_id'] == source_id[0]]
+
+                if chants_of_source != []:
+                    used_sources.append(source_id[0])
+                    source_chants_dict[source_id[0]] = chants_of_source
         
-        edges = []
-        edges_info = [(s1, s2, ch1, ch2, {'weight': w}) for s1, s2, ch1, ch2, w in zip(s1_column, s2_column, ch1_column, ch2_column, shared_column) if s1 != s2 and w != 0 and (s1 in used_sources and s2 in used_sources)]
-    
-    print('constructed')
+        used_sources = list(set(used_sources))
+        len_s = len(used_sources)
+        s1_column = [j for i in [len_s * [s] for s in used_sources] for j in i]
+        s2_column = len_s * used_sources
+
+        shared_column = []
+        ch1_column = []
+        ch2_column = []
+
+        if get_shared:
+            if compare_metrics == 'Jaccard':
+                for i in range(len(s1_column)):
+                    s1_chants = source_chants_dict[s1_column[i]]
+                    s2_chants = source_chants_dict[s2_column[i]]
+                    ch1_column.append(len(s1_chants))
+                    ch2_column.append(len(s2_chants))
+                    shared_column.append(Jaccard_metric(s1_chants, s2_chants))
+                edges = [(i, j, {'weight': round(w, 2) }) for i, j, w in zip(s1_column, s2_column, shared_column) if i != j and w != 0 and (i in used_sources and j in used_sources)]
+            
+            else: # Comparison distance based on topic model
+                with lzma.open('Map_repertoire/topic_models/dist_reduction.model', "rb") as model_file:
+                    trans = pickle.load(model_file)
+                    model = pickle.load(model_file)
+                
+                for i in range(len(s1_column)):
+                    s1_chants = source_chants_dict[s1_column[i]]
+                    s2_chants = source_chants_dict[s2_column[i]]
+                    ch1_column.append(len(s1_chants))
+                    ch2_column.append(len(s2_chants))
+                    trans_data = trans.transform([' '.join(s1_chants), ' '.join(s2_chants)])
+                    result = model.transform(trans_data)
+                    distance = jensenshannon(result[0], result[1]) #JensenShannon_metric(result[0], result[1])
+                    if np.isnan(distance):
+                        shared_column.append(0)
+                    else:
+                        shared_column.append(distance)
+                edges = [(i, j, {'weight': round(w, 2) }) for i, j, w in zip(s1_column, s2_column, shared_column) if i != j and w != 0 and (i in used_sources and j in used_sources)]
+
+            # Join obtained data into structure for graph and map
+            edges_info = [(s1, s2, ch1, ch2, {'weight': round(w, 2) }) for s1, s2, ch1, ch2, w in zip(s1_column, s2_column, ch1_column, ch2_column, shared_column) if s1 != s2 and w != 0 and (s1 in used_sources and s2 in used_sources)]
+        
+        else: # We have topic model based clustering
+            for i in range(len(s1_column)):
+                s1_chants = source_chants_dict[s1_column[i]]
+                s2_chants = source_chants_dict[s2_column[i]]
+                ch1_column.append(len(s1_chants))
+                ch2_column.append(len(s2_chants))
+                shared_column.append('-')
+            
+            edges = []
+            # Join obtained data into structure for graph and map
+            edges_info = [(s1, s2, ch1, ch2, {'weight': w}) for s1, s2, ch1, ch2, w in zip(s1_column, s2_column, ch1_column, ch2_column, shared_column) if s1 != s2 and w != 0 and (s1 in used_sources and s2 in used_sources)]
+
     return source_chants_dict, edges_info, edges, used_sources
 
 
@@ -212,7 +242,7 @@ def get_distance_matrix(source_chants_dict, used_sources, metric : str):
     if metric == 'Jaccard':
         for s_i in used_sources:
             for s_j in used_sources:
-                distance_matrix[source_dict[s_i], source_dict[s_j]] = 1 - Jaccard_metrics(source_chants_dict[s_i], source_chants_dict[s_j])
+                distance_matrix[source_dict[s_i], source_dict[s_j]] = 1 - Jaccard_metric(source_chants_dict[s_i], source_chants_dict[s_j])
     
     else: # Comparison based on topic model
         with lzma.open('Map_repertoire/topic_models/dist_reduction.model', "rb") as model_file:
@@ -222,10 +252,11 @@ def get_distance_matrix(source_chants_dict, used_sources, metric : str):
             for s_j in used_sources:
                 trans_data = trans.transform([' '.join(source_chants_dict[s_i]), ' '.join(source_chants_dict[s_j])])
                 result = model.transform(trans_data)
-                if np.isnan(jensenshannon(result[0], result[1])):
+                distance = jensenshannon(result[0], result[1])
+                if np.isnan(distance):
                     distance_matrix[source_dict[s_i], source_dict[s_j]] = 0
                 else:
-                    distance_matrix[source_dict[s_i], source_dict[s_j]] = jensenshannon(result[0], result[1])
+                    distance_matrix[source_dict[s_i], source_dict[s_j]] = distance
 
     return distance_matrix, used_sources
 
@@ -320,7 +351,6 @@ def get_communities(feast_ids : list[str], filtering_office : list[str], algorit
     returns communities found by Louvein algorithm, DBSCAN clustering or obtained from topic models 
     and info about edges of network to be drawn on the map as well as stability measure of clusterings
     """
-    print(add_info_algo)
     if algorithm == 'Louvein':
         communities, edges_info, sig_level = get_louvein_communities(feast_ids, filtering_office, add_info_algo)
     
